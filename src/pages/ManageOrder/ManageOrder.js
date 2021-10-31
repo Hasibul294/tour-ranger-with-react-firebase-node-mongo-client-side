cf77256dac6d8ce2c9fb936614b6fb1048a2af26
Hasibul294 <hasibulhasan294@gmail.com>
2021-10-31 19:15:43 +0600

    database and all functionality complete

--- src/pages/ManageOrder/ManageOrder.js
+++ src/pages/ManageOrder/ManageOrder.js
@@ -1,20 +1,90 @@
 import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
 
 const ManageOrder = () => {
-  const [orders, setOrders] = useState();
+  const [orders, setOrders] = useState([]);
   useEffect(() => {
     fetch("http://localhost:5000/orders")
       .then((res) => res.json())
       .then((data) => setOrders(data));
-  }, []);
+  }, [orders]);
+
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure, want to delete this order");
+    if (proceed) {
+      const url = `http://localhost:5000/orders/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          alert("Order Deleted Successfully");
+          const remainingOrder = orders.filter((order) => order._id !== id);
+          setOrders(remainingOrder);
+        });
+    }
+  };
+
+  const handleUpdate = (id) => {
+    const url = `http://localhost:5000/orders/${id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          alert("Booking Request Approved");
+        }
+      });
+  };
   return (
     <div className="container my-5 p-4">
       <h5 className="mb-3 text-color">Admin Panel</h5>
       <h2 className="mb-5">Manage All Orders</h2>
+      <h4 className="mb-3">Total Orders: {orders?.length}</h4>
       {orders?.map((order) => (
-        <li>
-          {order.name}::{order.email}::{order.status}
-        </li>
+        <div key={order._id} className="my-order">
+          <Row>
+            <Col lg={3}>
+              <p>{order.name}</p>
+            </Col>
+            <Col lg={3}>
+              <p>{order.email}</p>
+            </Col>
+            <Col lg={3}>
+              <p>{order.packageTitle}</p>
+            </Col>
+            <Col lg={2}>
+              {order.status === "confirm" ? (
+                <p className="text-success">Confirm</p>
+              ) : (
+                <p className="text-danger">
+                  Pending
+                  <span>
+                    <button
+                      className="border-0 rounded"
+                      onClick={() => handleUpdate(order._id)}
+                    >
+                      <i class="text-success fas fa-check-square"></i>
+                    </button>
+                  </span>
+                </p>
+              )}
+            </Col>
+            <Col lg={1}>
+              <button
+                className="border-0 rounded"
+                onClick={() => handleDelete(order._id)}
+              >
+                <i class="text-danger fas fa-trash-alt"></i>
+              </button>
+            </Col>
+          </Row>
+        </div>
       ))}
     </div>
   );
--- src/pages/MyOrders/MyOrders.js
+++ src/pages/MyOrders/MyOrders.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
-import { useHistory } from "react-router";
 import { Col, Row } from "react-bootstrap";
 import "./MyOrders.css";
 
 const MyOrders = () => {
   const [orders, setOrders] = useState([]);
   const { user } = useAuth();
-  const history = useHistory();
   const id = user.email;
   //   console.log(email);
   useEffect(() => {
@@ -17,50 +15,55 @@ const MyOrders = () => {
   }, [id]);
 
   const handleDelete = (id) => {
-    const url = `http://localhost:5000/orders/${id}`;
-    fetch(url, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
+    const proceed = window.confirm("Are you sure, want to delete this order");
+    if (proceed) {
+      const url = `http://localhost:5000/orders/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          alert("Order Deleted Successfully");
+          const remainingOrder = orders.filter((order) => order._id !== id);
+          setOrders(remainingOrder);
+        });
+    }
   };
+
   return (
     <div className="container my-5 p-4">
-      {user.email ? (
-        <div>
-          <h5>
-            Hello,
-            <span className="mb-3 text-color fs-6">{user.displayName}</span>
-          </h5>
-          <h2 className="mb-5">Your Order List</h2>
-          <h4 className="mb-3">Order item: {orders?.length}</h4>
-          {orders?.map((order) => (
-            <div key={order._id} className="my-order">
-              <Row>
-                <Col md={3}>
-                  <p>{order.name}</p>
-                </Col>
-                <Col md={3}>
-                  <p>{order.email}</p>
-                </Col>
-                <Col md={3}>
-                  <p>{order.packageTitle}</p>
-                </Col>
-                <Col md={2}>
-                  <p>{order.status}</p>
-                </Col>
-                <Col md={1}>
-                  <button onClick={() => handleDelete(order._id)}>X</button>
-                </Col>
-              </Row>
-            </div>
-          ))}
+      <h2 className="mb-5 text-color">Your Order List</h2>
+      <h4 className="mb-3">Order item: {orders?.length}</h4>
+      {orders?.map((order) => (
+        <div key={order._id} className="my-order">
+          <Row>
+            <Col lg={3}>
+              <p>{order.name}</p>
+            </Col>
+            <Col lg={3}>
+              <p>{order.email}</p>
+            </Col>
+            <Col lg={3}>
+              <p>{order.packageTitle}</p>
+            </Col>
+            <Col lg={2}>
+              {order.status === "confirm" ? (
+                <p className="text-success">Confirm</p>
+              ) : (
+                <p className="text-danger">Pending</p>
+              )}
+            </Col>
+            <Col lg={1}>
+              <button
+                className="border-0 rounded"
+                onClick={() => handleDelete(order._id)}
+              >
+                <i class="text-danger fas fa-trash-alt"></i>
+              </button>
+            </Col>
+          </Row>
         </div>
-      ) : (
-        history.push("/home")
-      )}
+      ))}
     </div>
   );
 };
